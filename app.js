4ae02c12623194aef4b97a824447b552752f91cc
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-12-06 13:55:45 -0300

    feat: ejs feature added

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
