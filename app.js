e88366c17618aad34e227ec5d8bed944b78b4cb8
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-12-06 16:29:12 -0300

    chore: changes to app.js

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.use(
-    express.static(path.join(__dirname, 'public'))
-);
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('index');
 });
 
 app.listen(PORT, () => {
