9979bde3ab6935324e18066aa9a608ce8d3c829e
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-12-11 16:42:16 -0300

    refactor: route code changes

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import books from './src/Controllers/books.js';
 
 const routes = express.Router();
@@ -14,13 +14,9 @@ routes.get('/register', (req, res) => {
 
 routes.post('/register', [
     body('title').notEmpty().withMessage('O campo "título" é obrigatório.'),
-    body('status').isEmail().withMessage('O campo "status" é obrigatório.')
-], (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.json({ errors: errors.array() });
-    }
-});
+    body('status').notEmpty().withMessage('O campo "status" é obrigatório.')
+], async (req, res) => {
+    await books.addBook(req, res);
+})
 
 export { routes as default };
