c23f64bdb0bcc5bd69fed0ea1f8f99c94a5103b5
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-12-11 13:49:15 -0300

    feat: add middleware to handle forms

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.set('view engine', 'ejs');
 app.set('views', './views');
