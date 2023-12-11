8c19050d0fd4aeb05ab330af492dec09c1959f47
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-12-11 14:21:32 -0300

    feat: add express-validator

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { body, validationResult } from 'express-validator';
 import books from './src/Controllers/books.js';
 
 const routes = express.Router();
@@ -9,6 +10,17 @@ routes.get('/', async (req, res) => {
 
 routes.get('/register', (req, res) => {
     res.render('register');
+});
+
+routes.post('/register', [
+    body('title').notEmpty().withMessage('O campo "título" é obrigatório.'),
+    body('status').isEmail().withMessage('O campo "status" é obrigatório.')
+], (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.json({ errors: errors.array() });
+    }
 });
 
 export { routes as default };
